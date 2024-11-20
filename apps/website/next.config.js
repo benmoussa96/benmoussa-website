36765f5dd023490a809c942c06b5d844a6f6ea85
ghaieth96 <ghaiethbmoussa@gmail.com>
2024-11-20 11:47:17 +0100

    build: removing disDir config

--- apps/website/next.config.js
+++ apps/website/next.config.js
@@ -3,6 +3,8 @@ const path = require("path");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: ["core"],
+  output: "standalone",
 };
 
 module.exports = nextConfig;
